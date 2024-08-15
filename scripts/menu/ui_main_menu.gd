dac8e2bc996a9ce10df97b5e517ced2899575d50
Rhythm <57643350+RhythmMakesGames@users.noreply.github.com>
2024-08-15 23:38:39 +0530

    menu work

--- scripts/menu/ui_main_menu.gd
+++ scripts/menu/ui_main_menu.gd
@@ -1,21 +1,26 @@
 extends Control
 
-@export var tutorial_scene = PackedScene
+@export var play_scene = PackedScene
 @export var options_scene = PackedScene
-@export var credits_scene = PackedScene
 
+
+func _ready() -> void:
+	$VBoxContainer/PlayButton.grab_focus()
 
 func _on_start_button_pressed() -> void:
 	# continue, if not running for the first time
 	if Global.current_level != null:
 		get_tree().change_scene_to_packed(Global.current_level)
 	else:
-		get_tree().change_scene_to_packed(tutorial_scene)
+		get_tree().change_scene_to_packed(play_scene)
 
 
 func _on_options_button_pressed() -> void:
 	get_tree().change_scene_to_packed(options_scene)
-	#get_tree().add_child(options)
+	
+	# the buttons still work
+	#var options = options_scene as PackedScene
+	#get_tree().current_scene.add_child(options.instantiate())
 
 
 func _on_quit_button_pressed() -> void:
